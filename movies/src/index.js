a1fac3aad5b1b92770cd15bacac6cae827d75f67
Leane <leaneleboulanger@gmail.com>
2023-10-10 13:52:50 +0100

    je peux affiché la page de detail maintenant

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import HomePage from "./pages/homePage";
+
+import HomePage from "./pages/homePage"; //home page
+import MovieDetailsPage from './pages/movieDetailsPage' //page de details des films
 
 const sample = {
   adult: false,
@@ -85,10 +87,17 @@ const sample = {
 
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
+
 const App = () => {
   return (
-      <HomePage movies={movies} />
-  );
+      <MovieDetailsPage movie={sample} images={images} />
+      );
 };
 
 const rootElement = createRoot( document.getElementById("root") )
